6c916c80b1fabfdd706f794d3536e1be1af4c255
kevin.tirta <7318377kevintirta@gmail.com>
2023-04-16 04:46:17 +0900

    [add] finally we made it RANNNN!!! YAY!

--- node/secured-finance.ts
+++ node/secured-finance.ts
@@ -1,10 +1,15 @@
-import { Signer, ethers, encodeBytes32String, BigNumberish, Provider } from "ethers";
-import { BN as BigNumber } from 'bn.js';
+import {
+  Signer,
+  ethers,
+  encodeBytes32String,
+  BigNumberish,
+  Provider,
+} from "ethers";
+import { BN as BigNumber } from "bn.js";
 
-import * as LendingMarketControllerABI from "../contractABI/LendingMarketController.json"
-import * as LendingMarketABI from "../contractABI/LendingMarket.json"
-import * as TokenVaultABI from "../contractABI/TokenVault.json"
-
+import * as LendingMarketControllerABI from "../contractABI/LendingMarketController.json" assert { type: "json" };
+import * as LendingMarketABI from "../contractABI/LendingMarket.json" assert { type: "json" };
+import * as TokenVaultABI from "../contractABI/TokenVault.json" assert { type: "json" };
 
 export class GasEstimator {
   private lendingMarketControllerContract: ethers.Contract;
@@ -22,26 +27,27 @@ export class GasEstimator {
       TokenVaultABI.default.abi,
       provider
     );
-  
-
   }
 
   // TODO: Implement this method
   public async estimateSwapFees() {
-    return BigInt(0)
+    return BigInt(0);
   }
 
   public async estimateLendingFee() {
     const gasLending = await this.provider.estimateGas({
       to: LendingMarketABI.default.address,
-      data: this.lendingMarketControllerContract.interface.encodeFunctionData("depositAndCreateOrder", [
-        encodeBytes32String("USDC"),
-        new BigNumber(Math.floor(Date.now() / 1000)),
-        0,
-        new BigNumber(0),
-        new BigNumber(0),
-      ])
-    })
+      data: this.lendingMarketControllerContract.interface.encodeFunctionData(
+        "depositAndCreateOrder",
+        [
+          encodeBytes32String("USDC"),
+          new BigNumber(Math.floor(Date.now() / 1000)),
+          0,
+          new BigNumber(0),
+          new BigNumber(0),
+        ]
+      ),
+    });
 
     // Additional 20% to be safe
     return gasLending * BigInt(1.2);
@@ -54,23 +60,27 @@ export class GasEstimator {
   public async estimateBorrowingFee(): Promise<bigint> {
     const gasDeposit = await this.provider.estimateGas({
       to: TokenVaultABI.default.address,
-      data: this.tokenVaultContract.interface.encodeFunctionData("deposit", [encodeBytes32String("USDC"), 1])
-    })
-    
+      data: this.tokenVaultContract.interface.encodeFunctionData("deposit", [
+        encodeBytes32String("USDC"),
+        1,
+      ]),
+    });
+
     const createDepositGas = await this.provider.estimateGas({
       to: LendingMarketControllerABI.default.address,
-      data: this.lendingMarketControllerContract.interface.encodeFunctionData("createOrder", [
-        encodeBytes32String("USDC"),
-        new BigNumber(Math.floor(Date.now() / 1000)),
-        1,
-        new BigNumber(0),
-        new BigNumber(0),
-      ])
-
-    })
+      data: this.lendingMarketControllerContract.interface.encodeFunctionData(
+        "createOrder",
+        [
+          encodeBytes32String("USDC"),
+          new BigNumber(Math.floor(Date.now() / 1000)),
+          1,
+          new BigNumber(0),
+          new BigNumber(0),
+        ]
+      ),
+    });
 
     // Additional 20% to be safe
     return (gasDeposit + createDepositGas) * BigInt(1.2);
   }
 }
-
